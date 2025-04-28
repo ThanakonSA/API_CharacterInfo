3a2cd74af7ed3f4b30d2772ebe264b602b739520
Thanakon.sa <119285334+ThanakonSA@users.noreply.github.com>
2025-04-28 15:15:26 +0700

    Update heromainmodel.py

--- heromainmodel.py
+++ heromainmodel.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 
-class HeroBase(BaseModel):
+class HeroMainModel(BaseModel):
     hero_id: str
     hero_name: str
     role: str
@@ -8,20 +8,29 @@ class HeroBase(BaseModel):
     lane_recc: str
     icon: str
     full: str
+    
+# class HeroBase(BaseModel):
+#     hero_id: str
+#     hero_name: str
+#     role: str
+#     specialty: str
+#     lane_recc: str
+#     icon: str
+#     full: str
 
-class HeroMainModel(BaseModel):
-    herobase: HeroBase
+# class HeroMainModel(BaseModel):
+#     herobase: HeroBase
 
 
-def convert_row_to_heroesmain(row: dict) -> dict:
-    return {
-        "herobase": {
-            "hero_id":    row.get("Hero_ID", ""),
-            "hero_name":  row.get("HeroName", ""),
-            "role":       row.get("Role", ""),
-            "specialty":  row.get("Specialty", ""),
-            "lane_recc":  row.get("Lane_Recc", ""),
-            "icon":       row.get("Iconhero", ""),
-            "full":       row.get("Imagehero", "")
-            }
-    }
+# def convert_row_to_heroesmain(row: dict) -> dict:
+#     return {
+#         "herobase": {
+#             "hero_id":    row.get("Hero_ID", ""),
+#             "hero_name":  row.get("HeroName", ""),
+#             "role":       row.get("Role", ""),
+#             "specialty":  row.get("Specialty", ""),
+#             "lane_recc":  row.get("Lane_Recc", ""),
+#             "icon":       row.get("Iconhero", ""),
+#             "full":       row.get("Imagehero", "")
+#             }
+#     }
