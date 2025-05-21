5877f6a222fbb1618ec262ec176a2459dbc74729
Thanakon.sa <thanakon.sa@kkumail.com>
2025-05-21 15:04:16 +0700

    update

--- models/Itemsfullmodel.py
+++ models/Itemsfullmodel.py
@@ -20,17 +20,23 @@ class ItemStats(BaseModel):
     mana_regen:        str
     physical_defense:  str
     magic_defense:     str
-    lifesteal:         str
-    spell_vamp:        str
-    hybrid_lifesteal:  str
-    cooldown_reduction:str
-    attack_speed:      str
+    lifesteal_:         str
+    spell_vamp_:        str
+    hybrid_lifesteal_:  str
+    cooldown_reduction_:str
+    attack_speed_:      str
     adaptive_attack:   str
-    magic_penetration: str
-    critical_chance:   str
-    critical_damage:   str
+    adaptive_attack_:   str
+    physical_penetration: str
+    physical_penetration_:str
+    magic_penetration: str 
+    magic_penetration_:str
+    critical_chance_:   str
+    critical_damage_:   str
     movement_speed:    str
-    slow_reduction:    str
+    movement_speed_:    str
+    slow_reduction_:    str
+    healing_effect_:   str
 
 class ItemFullModel(BaseModel):
     iteminfo:  ItemInfo
@@ -40,10 +46,6 @@ class ItemFullModel(BaseModel):
 class ItemMainModel(BaseModel):
     iteminfo:  ItemInfo
 
-# class ItemBuild(BaseModel):
-#     hero_id:      str
-#     hero_name:    str
-#     items:        List[ItemMainModel]
 
 def convert_row_to_item(row: dict) -> dict:
     return {
@@ -60,24 +62,31 @@ def convert_row_to_item(row: dict) -> dict:
             "passive":    row.get("Passive", ""),
         },
         "stats": {
-            "physical_attack":    row.get("Physical_Attack", ""),
-            "magic_power":        row.get("Magic_Power", ""),
-            "hp":                 row.get("HP", ""),
-            "hp_regen":           row.get("HP_Regen", ""),
-            "mana":               row.get("Mana", ""),
-            "mana_regen":         row.get("Mana_Regen", ""),
-            "physical_defense":   row.get("Physical_Defense", ""),
-            "magic_defense":      row.get("Magic_Defense", ""),
-            "lifesteal":          row.get("Lifesteal", ""),
-            "spell_vamp":         row.get("Spell_Vamp", ""),
-            "hybrid_lifesteal":   row.get("Hybrid_Lifesteal", ""),
-            "cooldown_reduction": row.get("Cooldown_Reduction", ""),
-            "attack_speed":       row.get("Attack_Speed", ""),
-            "adaptive_attack":    row.get("Adaptive_Attack", ""),
-            "magic_penetration":  row.get("Magic_Penetration", ""),
-            "critical_chance":    row.get("Critical_Chance", ""),
-            "critical_damage":    row.get("Critical_Damage", ""),
-            "movement_speed":     row.get("Movement_Speed", ""),
-            "slow_reduction":     row.get("Slow_Reduction", ""),
+            "physical_attack":              row.get("Physical_Attack", ""),
+            "magic_power":                  row.get("Magic_Power", ""),
+            "hp":                           row.get("HP", ""),
+            "hp_regen":                     row.get("HP_Regen", ""),
+            "mana":                         row.get("Mana", ""),
+            "mana_regen":                   row.get("Mana_Regen", ""),
+            "physical_defense":             row.get("Physical_Defense", ""),
+            "magic_defense":                row.get("Magic_Defense", ""),
+            "lifesteal_":                   row.get("Lifesteal%", ""),
+            "spell_vamp_":                  row.get("Spell_Vamp%", ""),
+            "hybrid_lifesteal_":            row.get("Hybrid_Lifesteal%", ""),
+            "cooldown_reduction_":          row.get("Cooldown_Reduction%", ""),
+            "attack_speed_":                row.get("Attack_Speed%", ""),
+            "adaptive_attack":              row.get("Adaptive_Attack", ""),
+            "adaptive_attack_":             row.get("Adaptive_Attack%", ""),
+            "physical_penetration":         row.get("Physical_Penetration", ""),
+            "physical_penetration_":        row.get("Physical_Penetration%", ""),
+            "magic_penetration":            row.get("Magic_Penetration", ""),
+            "magic_penetration_":           row.get("Magic_Penetration%", ""),
+            "critical_chance_":             row.get("Critical_Chance%", ""),
+            "critical_damage_":             row.get("Critical_Damage%", ""),
+            "critical_damage_reduction_":   row.get("Critical_Damage_Reduction%", ""),
+            "movement_speed":               row.get("Movement_Speed", ""),
+            "movement_speed_":              row.get("Movement_Speed%", ""),
+            "slow_reduction_":              row.get("Slow_Reduction%", ""),
+            "healing_effect_":              row.get("Healing_Effect%", ""),
         }
     }
